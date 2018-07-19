3bcfa8bf7d96b1626f9da174e08b5e204d2e4942
daltonbr <daltonvarussa@gmail.com>
2018-07-19 22:33:24 +0000

    spawn piece backgrounds

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,15 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class Grid : MonoBehaviour {
+public class Grid : MonoBehaviour
+{
+    public enum PieceType
+    {
+        NORMAL,
+        COUNT,
+    };
 
-	// Use this for initialization
-	void Start () {
+    [System.Serializable]
+    public struct PiecePrefab
+    {
+        public PieceType type;
+        public GameObject prefab;
+    };
+
+    public int xDim;
+    public int yDim;
+
+    public PiecePrefab[] piecePrefabs;
+    public GameObject backgroundPrefab;
+
+    private Dictionary<PieceType, GameObject> piecePrefabDict;
 	
+	void Start ()
+    {
+        piecePrefabDict = new Dictionary<PieceType, GameObject>();
+        for (int i = 0; i < piecePrefabs.Length; i++)
+        {
+            if (!piecePrefabDict.ContainsKey(piecePrefabs[i].type))
+            {
+                piecePrefabDict.Add(piecePrefabs[i].type, piecePrefabs[i].prefab);
+            }
+        }
+
+        for (int x = 0; x < xDim; x++)
+        {
+            for (int y = 0; y < yDim; y++)
+            {
+                GameObject background = (GameObject)Instantiate(backgroundPrefab, new Vector3(x, y, 0), Quaternion.identity);
+                background.transform.parent = transform;
+            }
+        }
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
