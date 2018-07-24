040e15f1e4fe1b354ff39b99127ad98c5303c887
Dalton Lima <daltonvarussa@gmail.com>
2018-07-24 00:13:52 -0300

    Fill the board

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -59,8 +59,65 @@ public class Grid : MonoBehaviour
                 SpawnNewPiece(x, y, PieceType.EMPTY);
             }
         }
+
+        Fill();
 	}
     
+    public void Fill()
+    {
+        while (FillStep()) {}
+    }
+
+    /// <summary>
+    /// One pass through all grid cells, moving them down one grid, if possible.
+    /// </summary>
+    /// <returns> returns true if at least one piece is moved down</returns>
+    public bool FillStep()
+    {
+        bool movedPiece = false;
+        // y = 0 is at the top, we ignore the last row, since it can't be moved down.
+        for (int y = yDim - 2; y >= 0; y--)
+        {
+            for (int x = 0; x < xDim; x++)
+            {
+                GamePiece piece = pieces[x, y];
+
+                if (piece.IsMovable())
+                {
+                    GamePiece pieceBelow = pieces[x, y + 1];
+
+                    if (pieceBelow.Type == PieceType.EMPTY)
+                    {
+                        piece.MovableComponent.Move(x, y + 1);
+                        pieces[x, y + 1] = piece;
+                        SpawnNewPiece(x, y, PieceType.EMPTY);
+                        movedPiece = true;
+                    }
+                }
+            }
+        }
+
+        // the highest row (0) is a special case, we must fill it with new pieces if empty
+        for (int x = 0; x < xDim; x++)
+        {
+            GamePiece pieceBelow = pieces[x, 0];
+
+            if (pieceBelow.Type == PieceType.EMPTY)
+            {
+                GameObject newPiece = (GameObject)Instantiate(piecePrefabDict[PieceType.NORMAL], GetWorldPosition(x, -1), Quaternion.identity, this.transform);
+                //newPiece.transform.parent = transform;
+
+                pieces[x, 0] = newPiece.GetComponent<GamePiece>();
+                pieces[x, 0].Init(x, -1, this, PieceType.NORMAL);
+                pieces[x, 0].MovableComponent.Move(x, 0);
+                pieces[x, 0].ColorComponent.SetColor((ColorPiece.ColorType)Random.Range(0, pieces[x, 0].ColorComponent.NumColors));
+                movedPiece = true;
+            }
+        }
+
+        return movedPiece;
+    }
+
     public Vector2 GetWorldPosition (int x, int y)
     {
         return new Vector2(transform.position.x - xDim / 2.0f + x,
