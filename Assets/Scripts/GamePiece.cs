a56419e9857cd46336824f9294d58d5b0db6a210
Dalton Lima <daltonvarussa@gmail.com>
2018-07-23 21:49:19 -0300

    Add GamePiece class

---
+++ Assets/Scripts/GamePiece.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GamePiece : MonoBehaviour
+{
+
+    private int x;
+    private int y;
+
+    public int X
+    {
+        get { return x; }
+    }
+
+    public int Y
+    {
+        get { return y; }
+    }
+
+    private Grid.PieceType type;
+
+    public Grid.PieceType Type
+    {
+        get { return type; }
+    }
+
+    private Grid grid;
+
+    public Grid GridRef
+    {
+        get { return grid; }
+    }
+
+    public void Init(int _x, int _y, Grid _grid, Grid.PieceType _type)
+    {
+        x = _x;
+        y = _y;
+        grid = _grid;
+        type = _type;
+    }
+}
--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -25,7 +25,7 @@ public class Grid : MonoBehaviour
 
     private Dictionary<PieceType, GameObject> piecePrefabDict;
 
-    private GameObject[,] pieces;
+    private GamePiece[,] pieces;
 	
 	void Start ()
     {
@@ -47,14 +47,17 @@ public class Grid : MonoBehaviour
             }
         }
 
-        pieces = new GameObject[xDim, yDim];
+        pieces = new GamePiece[xDim, yDim];
         for (int x = 0; x < xDim; x++)
         {
             for (int y = 0; y < yDim; y++)
             {
-                pieces[x, y] = (GameObject)Instantiate(piecePrefabDict[PieceType.NORMAL], GetWorldPosition(x,y), Quaternion.identity);
-                pieces[x, y].name = "Piece(" + x + "," + y + ")";
-                pieces[x, y].transform.parent = transform;
+                GameObject newPiece = (GameObject)Instantiate(piecePrefabDict[PieceType.NORMAL], GetWorldPosition(x,y), Quaternion.identity);
+                newPiece.name = "Piece(" + x + "," + y + ")";
+                newPiece.transform.parent = transform;
+
+                pieces[x, y] = newPiece.GetComponent<GamePiece>();
+                pieces[x, y].Init(x, y, this, PieceType.NORMAL);
             }
         }
 	}
