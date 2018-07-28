6c08d693b56d9cdc61fd0e83ae98f871d5cc4a38
Dalton Lima <daltonvarussa@gmail.com>
2018-07-28 15:40:09 -0300

    Script the level base class

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class Level : MonoBehaviour
+{
+
+    public enum LevelType
+    {
+        TIMER,
+        OBSTACLE,
+        MOVES,
+    };
+
+    public Grid grid;
+
+    public int score1Star;
+    public int score2Star;
+    public int score3Star;    
+
+    protected LevelType type;
+
+    protected int currentScore;
+
+    public LevelType Type
+    {
+        get { return type; }
+    }
+
+    public virtual void GameWin()
+    {
+        Debug.Log("You Win!");
+    }
+
+    public virtual void GameLose()
+    {
+        Debug.Log("You Lose!");
+    }
+    
+    public virtual void OnMove()
+    {
+
+    }
+
+    public virtual void OnPieceCleared(GamePiece piece)
+    {
+        // Update score
+    }
+
+}
