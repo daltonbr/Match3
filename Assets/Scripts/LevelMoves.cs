c5d98fb455b6f4e5b7f9dc0bd3e51fa299e46296
Dalton Lima <daltonvarussa@gmail.com>
2018-07-28 17:41:19 -0300

    Number of moves level type

---
+++ Assets/Scripts/LevelMoves.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class LevelMoves : Level
+{
+
+    public int numMoves;
+    public int targetScore;
+
+    private int movesUsed = 0;
+
+    void Start()
+    {
+        type = LevelType.MOVES;
+
+        Debug.Log("Number of moves: " + numMoves +
+                  " Target score: " + targetScore);
+    }
+
+    public override void OnMove()
+    {
+        movesUsed++;
+
+        Debug.Log("Moves remaining: " + (numMoves - movesUsed));
+
+        if (numMoves - movesUsed == 0)
+        {
+            if (currentScore >= targetScore)
+            {
+                GameWin();
+            }
+            else
+            {
+                GameLose();
+            }
+        }
+    }
+}
