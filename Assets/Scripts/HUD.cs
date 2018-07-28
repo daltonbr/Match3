15cc6a8340328ec897646c6083f18edd88875fee
Dalton Lima <daltonvarussa@gmail.com>
2018-07-28 19:05:57 -0300

    The HUD script

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    // TODO get this automatically and/or make an assertion
+    public Level level;
+
+    public Text remainingText;
+    public Text remainingSubText;
+    public Text targetText;
+    public Text targetSubtext;
+    public Text scoreText;
+    public Image[] stars;
+
+    private int starIdx = 0;
+
+    private bool isGameOver = false;
+
+    void Start ()
+	{
+		
+	}
+
+}
