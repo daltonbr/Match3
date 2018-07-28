33bca541b875feec501bacb985388e1468d347c5
Dalton Lima <daltonvarussa@gmail.com>
2018-07-28 19:24:37 -0300

    Implement the HUD functions

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -21,7 +22,95 @@ public class HUD : MonoBehaviour
 
     void Start ()
 	{
-		
+	    for (int i = 0; i < stars.Length; i++)
+	    {
+	        if (i == starIdx)
+	        {
+	            stars[i].enabled = true;
+	        }
+	        else
+	        {
+	            stars[i].enabled = false;
+	        }
+	    }
 	}
 
+    public void SetScore(int score)
+    {
+        scoreText.text = score.ToString();
+
+        int visibleStar = 0;
+
+        if (score >= level.score1Star && score < level.score2Star)
+        {
+            visibleStar = 1;
+        }
+        else if  (score >= level.score2Star && score < level.score3Star)
+        {
+            visibleStar = 2;
+        }
+        else if (score >= level.score3Star)
+        {
+            visibleStar = 3;
+        }
+
+        for (int i = 0; i < stars.Length; i++)
+        {
+            stars[i].enabled = (i == visibleStar);
+            //if (i == visibleStar)
+            //{
+            //    stars[i].enabled = true;
+            //}
+            //else
+            //{
+            //    stars[i].enabled = false;
+            //}
+        }
+
+        starIdx = visibleStar;
+    }
+
+    public void SetTarget(int target)
+    {
+        targetText.text = target.ToString();
+    }
+
+    public void SetRemaining(int remaining)
+    {
+        remainingText.text = remaining.ToString();
+    }
+
+    public void SetRemaining(string remaining)
+    {
+        remainingText.text = remaining;
+    }
+
+    public void SetLevelType(Level.LevelType type)
+    {
+        if (type == Level.LevelType.MOVES)
+        {
+            remainingSubText.text = "moves remaining";
+            targetSubtext.text = "target score";
+        }
+        else if (type == Level.LevelType.OBSTACLE)
+        {
+            remainingSubText.text = "moves remaining";
+            targetSubtext.text = "bubbles remaining";
+        }
+        else if (type == Level.LevelType.TIMER)
+        {
+            remainingSubText.text = "time remaining";
+            targetSubtext.text = "target score";
+        }
+    }
+
+    public void OnGameWin(int score)
+    {
+        isGameOver = true;
+    }
+
+    public void OnGameLose(int score)
+    {
+        isGameOver = true;
+    }
 }
