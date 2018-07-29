f0ed936eca1a8c7971fb649ff043f306188c1205
Dalton Lima <daltonvarussa@gmail.com>
2018-07-28 22:19:29 -0300

    Script the game over screen

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    public GameObject screenParent;
+    public GameObject scoreParent;
+    public Text loseText;
+    public Text scoreText;
+    public Image[] stars;
+
+    void Start ()
+	{
+		screenParent.SetActive(false);
+
+	    for (int i = 0; i < stars.Length; i++)
+	    {
+	        stars[i].enabled = false;
+	    }
+    }
+
+    public void ShowLose()
+    {
+        screenParent.SetActive(true);
+        scoreParent.SetActive(false);
+
+        Animator animator = GetComponent<Animator>();
+
+        if (animator)
+        {
+            animator.Play("GameOverShow");
+        }
+    }
+
+    public void ShowWin(int score, int starCount)
+    {
+        screenParent.SetActive(true);
+        loseText.enabled = false;
+
+        scoreText.text = score.ToString();
+        scoreText.enabled = false;
+
+        Animator animator = GetComponent<Animator>();
+
+        if (animator)
+        {
+            animator.Play("GameOverShow");
+        }
+
+        StartCoroutine(ShowWinCoroutine(starCount));
+    }
+
+    private IEnumerator ShowWinCoroutine(int starCount)
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        if (starCount < stars.Length)
+        {
+            for (int i = 0; i <= starCount; i++)
+            {
+                stars[i].enabled = true;
+
+                if (i > 0)
+                {
+                    stars[i - 1].enabled = false;
+                }
+
+                yield return new WaitForSeconds(0.5f);
+            }
+        }
+
+        scoreText.enabled = true;
+    }
+
+    // TODO reference these methods via code
+    public void OnReplayClicked()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
+
+    public void OnDoneClicked()
+    {
+        // TODO Done
+    }
+
+}
